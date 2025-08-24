473f73298d4e905c9aebf4688e7fbcc871352042
Ariabagus Wisnuwardhana Mustikoatmojo <4riabagus@gmail.com>
2025-08-24 16:03:11 +0700

    addtional features binning for oscillators indicators

--- technicalIndicators/oscillators.py
+++ technicalIndicators/oscillators.py
@@ -7,6 +7,13 @@ def calculate_relative_strength_index(prepared_data):
         'Date': [val.date for val in result],
         'Relative Strength Index': [val.rsi for val in result]
     })
+    
+    result_df.dropna(subset=['Relative Strength Index'], inplace=True)
+    
+    result_df['RSI Up Trend'] = (result_df['Relative Strength Index'] >= 65).astype(int)
+    result_df['RSI Down Trend'] = (result_df['Relative Strength Index'] <= 35).astype(int)
+
+    result_df.drop(columns=['Relative Strength Index'], inplace=True)
 
     return result_df.set_index('Date')
 
@@ -14,9 +21,14 @@ def calculate_stochastic_oscillator(prepared_data):
     result = indicators.get_stoch(prepared_data)
     result_df = pd.DataFrame({
         'Date': [val.date for val in result],
-        'Stochastic Oscilator': [val.oscillator for val in result],
-        'Signal Stochastic Oscilator': [val.signal for val in result],
-        'Percent Stochastic Oscilator': [val.percent_j for val in result],
+        'Stochastic Oscillator': [val.oscillator for val in result]
     })
+
+    result_df.dropna(subset=['Stochastic Oscillator'], inplace=True)
+
+    result_df['Stochastic Oscillator Overbought'] = (result_df['Stochastic Oscillator'] >= 80).astype(float)
+    result_df['Stochastic Oscillator Oversold'] = (result_df['Stochastic Oscillator'] <= 20).astype(float)
+
+    result_df.drop(columns=['Stochastic Oscillator'], inplace=True)
 
     return result_df.set_index('Date')
